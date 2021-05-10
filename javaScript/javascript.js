2bb31118319edb630b50ee06879ab44b5eb7bf07
v203 <vuyisamr203@gmail.com>
2021-05-10 14:41:12 +0200

    14:41

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -1,1 +1,17 @@
+function openProj(evt,projName){
+    var i, tabcontent,tablinks
 
+    tabcontent=document.getElementsByClassName("tabcontent");
+
+    for(i=0;i<tabcontent.length;i++){
+        tabcontent[i].style.display= none;
+    }
+
+    tablinks=document.getElementsByClassName("tablinks");
+    for(i=0;tablinks.length;i++){
+
+        tablinks[i].className=tablinks.className.replace("active","")
+    }
+    document.getElementById(projName).style.display=block;
+    evt.currentTarget.className += " active";
+}
